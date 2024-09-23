8235e360b8eef83bc1c4795556a6039eca12a849
emon22830 <mdemon22830@gmail.com>
2024-09-23 16:14:26 +0800

    part 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,47 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('assets/logo.png'),
+                    height: 60,
+                    width: 60,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Maintainance',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Rubik Medium',
+                          color: Color(0xff203142),
+                        ),
+                      ),
+                      Text(
+                        'Box',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Rubik Medium',
+                          color: Color(0xffF97030),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 40,
+              ),
               Center(
                 child: Text(
                   'Login',
@@ -25,18 +66,35 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(height: 15),
               Center(
                 child: Text(
                   'Get Numerious Adventure From\n A Single App',
                   textAlign: TextAlign.center,
-maxLines: 2,
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 18,
                     fontFamily: 'Rubik Regular',
                     color: Color(0xff405988),
                   ),
                 ),
               ),
+              Container(
+                height: 50,
+                width: 300,
+
+                child: Center(
+                  child: Text('Login',style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Rubik Regular',
+                    color: Colors.white,
+                  ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffF97038),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
             ],
           ),
         ),
