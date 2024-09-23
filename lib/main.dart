93667114d12fef1a931bd07608d31256b66276fe
emon22830 <mdemon22830@gmail.com>
2024-09-23 16:57:28 +0800

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class MyApp extends StatelessWidget {
               SizedBox(height: 15),
               Center(
                 child: Text(
-                  'Get Numerious Adventure From\n A Single App',
+                  'Get Numerous Adventure From\n A Single App',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 18,
@@ -78,23 +78,103 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.redAccent,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff323F38)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  obscureText: true, // Hide the password input
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: Color(0xffF8F9FA),
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.redAccent,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff323F38)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
               Container(
                 height: 50,
                 width: 300,
-
                 child: Center(
-                  child: Text('Login',style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'Rubik Regular',
-                    color: Colors.white,
-                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Rubik Regular',
+                      color: Colors.white,
+                    ),
                   ),
                 ),
                 decoration: BoxDecoration(
                   color: Color(0xffF97038),
                   borderRadius: BorderRadius.circular(10),
                 ),
-              )
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Don't have an account? ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: 'Rubik Regular',
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: TextStyle(
+                            color: Color(0xffF97030), // Fixed hex color code
+                            fontSize: 16,
+                            fontFamily: 'Rubik Medium',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
